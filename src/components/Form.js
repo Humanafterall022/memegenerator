791c0029e444e32f5197f0498ffb1e58abafd034
Miguel Tinta <mtinta@unsa.edu.pe>
2023-03-05 23:44:31 -0500

    FinalMemeGen

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,22 +2,24 @@ import React from "react";
 import memesData from "../memesData";
 export default function Form(){
 
-    
-let url
-    function refreshImage(){
+    const [memeImage, setMemeImage]=React.useState("")
+
+    function getMemeImage(){
         const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random()  * memesArray.length )
-        url= memesArray[randomNumber].url
-        console.log(url)
+        const randomNumber = Math.floor(Math.random()*memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
     }
+
+   
 
 
     return(
         <div className="form">
             <input type="text" placeholder="Arriba" className="form--text"/>
             <input type="text" placeholder="Abajo" className="form--text"/>
-            <button className="form--button" onClick={refreshImage}>Get a new meme image</button>
-            <img src={url}/>
+            <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
+            
+            <img src={memeImage} className="form--memeimage"/>
         </div>
     )
 }
